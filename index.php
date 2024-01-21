31e883cfbbe3e811b1134d4cb86e69a917c518a7
F.Orellana <fernando_ariel1995@hotmail.com>
2024-01-21 18:55:36 -0300

    cambie algo de index

--- index.php
+++ index.php
@@ -49,7 +49,8 @@
                         <div class="contenedor-cosas-imagen"></div>
                     </div>
                     <div class="contenedor-cosas-abajo">
-                        <form class="formulario" onsubmit="return validarFormulario()">
+                        <form class="formulario" onsubmit="return validarFormulario() ">
+                            <!-- Tus campos de formulario aquí -->
                             <!-- Tus campos de formulario aquí -->
                             <input type="text" id="nombre" name="nombre" placeholder="nombre">
                             <input type="email" id="email" name="email" placeholder="email">
